d52eabb3d7780c620c988290de865def4a16f8f2
gogoday <wsf_1234567@126.com>
2015-10-27 21:15:18 +0800

    second

--- javascripts/post.js
+++ javascripts/post.js
@@ -2,7 +2,6 @@ function getLeft(dom) {
     var p, left = 0;
     p = dom.offsetParent;
     while (p.tagName.toLowerCase() != 'body') {
-        console.log(p.className + ':' + p.offsetLeft);
         left += p.offsetLeft;
         p = p.offsetParent;
     }
